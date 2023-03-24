e0f2051bb7d5b4cf2b04851713c9d4bfb5bf4c81
BigGSwimmer <128221950+BigGSwimmer@users.noreply.github.com>
2023-03-24 09:43:31 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+basic.showIcon(IconNames.Happy)
